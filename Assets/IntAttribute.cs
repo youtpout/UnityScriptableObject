39a404c5627f4cdce3f5835333e0f9f119400e79
Eddy Boughioul <eddyboughioul@hotmail.fr>
2021-01-23 15:10:17 +0100

    Exemple of project with scriptableobject

---
+++ Assets/IntAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "NewIntAttribute", menuName = "CustomSO/Attribute/IntAttribute")]
+public class IntAttribute : ScriptableObject
+{
+    [SerializeField]
+    private int value;
+    public int Value
+    {
+        get
+        {
+            return value;
+        }
+        set
+        {
+            this.value = value;
+            NotifyValueChanged();
+        }
+    }
+
+    [Header("Value that will set on calling reset method")]
+    public int InitialValue;
+
+    public void ResetToInitialValue()
+    {
+        Value = InitialValue;
+    }
+
+    public UnityAction OnValueChanged;
+    private void NotifyValueChanged()
+    {
+        OnValueChanged?.Invoke();
+    }
+
+}
---
+++ Assets/ShowScriptable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScriptable : MonoBehaviour
+{
+    public IntAttribute IntegerUpdatable;
+    public bool NotifyPropertyChange = true;
+    public string Label = "Value";
+
+    private Text m_Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Text = GetComponent<Text>();
+        if (IntegerUpdatable != null && NotifyPropertyChange)
+        {
+            IntegerUpdatable.OnValueChanged = UpdateText;
+        }
+        m_Text.text = Label + " " + IntegerUpdatable.Value.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateText()
+    {
+        m_Text.text = Label + " " + IntegerUpdatable.Value.ToString();
+    }
+}
